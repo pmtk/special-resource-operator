ead7c46f34f3761b5c789b044e8703e92a19bf2d
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2022-02-25 12:58:02 +0100

    Error if cannot find cache location

--- pkg/helmer/helmer.go
+++ pkg/helmer/helmer.go
@@ -16,6 +16,7 @@ import (
 	helmerv1beta1 "github.com/openshift-psap/special-resource-operator/pkg/helmer/api/v1beta1"
 	"github.com/openshift-psap/special-resource-operator/pkg/resource"
 	"github.com/openshift-psap/special-resource-operator/pkg/utils"
+	"github.com/pkg/errors"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/chart/loader"
@@ -32,13 +33,12 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
 
-func DefaultSettings() *cli.EnvSettings {
+func DefaultSettings() (*cli.EnvSettings, error) {
 	s := cli.New()
 
 	cacheDir, err := os.UserCacheDir()
 	if err != nil {
-		// Expected path for SRO in container
-		cacheDir = "/root/nonroot/.cache"
+		return nil, errors.Wrap(err, "failed to obtain a cache directory")
 	}
 
 	s.RepositoryConfig = filepath.Join(cacheDir, "special-resource-operator/helm/repositories/config.yaml")
@@ -47,7 +47,7 @@ func DefaultSettings() *cli.EnvSettings {
 	s.Debug = true
 	s.MaxHistory = 10
 
-	return s
+	return s, nil
 }
 
 func OpenShiftInstallOrder() {
